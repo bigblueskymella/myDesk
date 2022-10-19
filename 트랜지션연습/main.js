4c48f61abc43a7f8e5363d100e8c6b7975f09c29
bigblueskymella <110326250+bigblueskymella@users.noreply.github.com>
2022-10-19 22:41:55 +0900

    Add files via upload

---
+++ 트랜지션연습/main.js
@@ -0,0 +1,17 @@
+// let moveP = document.querySelector("p");
+// let moveS = document.querySelector("span");
+
+// window.addEventListener('scroll',function(){
+//     let how = this.window.scrollY
+//     console.log("scrollY", how)
+
+//     if(how > 300){
+//         moveP.style.animation = 'moveOut 1s ease-out forwards';
+//         moveS.style.animation = 'moveOut 2s ease';
+//     }else{
+//         moveP.style.animation = 'moveIn 1s ease-out'
+//         moveS.style.animation = 'moveIn 2s ease'
+//     }
+// });
+
+// //forwards 안 넣으면 다시 돌아옴!!
